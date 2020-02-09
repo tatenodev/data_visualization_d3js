91b8050f7036000be49ec84e7b05057e0f40cc6a
wd30gsrc <gd28.c.tateno@gmail.com>
2020-02-09 15:12:21 +0900

    uppercase

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 import { getCars } from './getCars.js';
 import { generateReport } from './generateReport.js';
 
-document.getElementById('message-element').textContent = 'loading...';
+document.getElementById('message-element').textContent = 'Loading...';
 
 getCars().then(cars => {
   const message = generateReport(cars, 2000);
