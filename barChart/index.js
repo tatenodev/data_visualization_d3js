698b7f333306d5b63672c2a74cc37cb7c6dea81d
wd30gsrc <gd28.c.tateno@gmail.com>
2020-02-11 20:33:22 +0900

    不要ファイル削除

--- barChart/index.js
+++ barChart/index.js
@@ -3,6 +3,16 @@ const svg = d3.select('svg');
 const width = +svg.attr('width');
 const height = +svg.attr('height');
 
+const render = data => {
+  svg.selectAll('rect').data(data)
+  .enter().append('rect')
+    .attr('width', 300)
+    .attr('height', 300)
+};
+
 d3.csv('data/data.csv').then(data => {
-  console.log(data);
-})
+  data.forEach(d => {
+    d.population = +d.population * 1000;
+  });
+  render(data);
+});
