7d81c21b472189000db5bb6b58bb38a649527400
wd30gsrc <gd28.c.tateno@gmail.com>
2020-02-11 18:30:22 +0900

    更新：円の属性をsvgからgに変更

--- shaps_svg_css/index.js
+++ shaps_svg_css/index.js
@@ -3,10 +3,11 @@ const svg = d3.select('svg');
 const width = +svg.attr('width');
 const height = +svg.attr('height');
 
-const circle = svg.append('circle')
+const g = svg.append('g')
+  .attr('transform', `translate(${width/2},${height/2})`);
+
+const circle = g.append('circle')
   .attr('r', height / 2)
-  .attr('cx', width / 2)
-  .attr('cy', 500/2)
   .attr('fill', 'yellow')
   .attr('stroke', 'black');
 
@@ -26,9 +27,6 @@ const rightEye = svg.append('circle')
 .attr('cy', 500/2 + eyeYOffset)
 .attr('fill', 'yellow')
 .attr('fill', 'black');
-
-const g = svg.append('g')
-  .attr('transform', `translate(${width/2},${height/2})`);
 
 const mouth = g.append('path')
   .attr('d', d3.arc()({
