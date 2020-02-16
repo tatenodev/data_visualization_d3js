2d286fe7f30b1e7e06d2b3e519c834eecaabb0d3
wd30gsrc <gd28.c.tateno@gmail.com>
2020-02-16 20:01:16 +0900

    表の形に整形

--- barChart/index.js
+++ barChart/index.js
@@ -4,16 +4,22 @@ const width = +svg.attr('width');
 const height = +svg.attr('height');
 
 const render = data => {
+  const xValue = d => d.population;
+  const yValue = d => d.country;
+
   const xScale = d3.scaleLinear()
-    .domain([0, d3.max(data, d => d.population)])
+    .domain([0, d3.max(data, xValue)])
     .range([0, width]);
   
-  console.log(xScale.range());
+  const yScale = d3.scaleBand()
+    .domain(data.map(yValue))
+    .range([0, height]);
 
   svg.selectAll('rect').data(data)
   .enter().append('rect')
-    .attr('width', d => xScale(d.population))
-    .attr('height', 300)
+    .attr('y', d => yScale(yValue(d)))
+    .attr('width', d => xScale(xValue(d)))
+    .attr('height', yScale.bandwidth());
 };
 
 d3.csv('data/data.csv').then(data => {
